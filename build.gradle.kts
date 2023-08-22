5d71a6d8fc87147b23b58e1636a13816ae2dc3b5
Matheus Morillo <matheus.morillo2000@gmail.com>
2023-08-22 20:23:25 -0300

    add publish plugin and add projet bare bones

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
 	java
-	id("org.springframework.boot") version "3.1.2"
+	`maven-publish`
 	id("io.spring.dependency-management") version "1.1.2"
+	id("com.github.davidmc24.gradle.plugin.avro") version "1.8.0"
+	id("com.intershop.gradle.scmversion") version "6.2.0"
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
+scm {
+	version {
+		type = "threeDigits"
+		initialVersion = "0.1.0"
+	}
+}
+
+group = "com.matheus"
+version = scm.version.version
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
@@ -15,14 +24,28 @@ repositories {
 	mavenCentral()
 }
 
+publishing {
+	publications {
+		create<MavenPublication>("maven") {
+			artifactId = "ecommerce-messaging-schema-registry"
+			from(components["java"])
+		}
+	}
+
+	repositories {
+		mavenLocal()
+	}
+}
+
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
-	implementation("org.springframework.boot:spring-boot-starter-web:3.1.2")
-	implementation("org.springframework.kafka:spring-kafka:3.0.9")
+	implementation("org.springframework:spring-messaging:6.0.6")
 	implementation("org.apache.avro:avro:1.11.2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+tasks.withType<Jar> {
+	archiveBaseName.set("ecommerce-messaging-schema-registry")
 }
---
+++ src/main/java/com/example/ecommercemessagingschemaregistry/MessageBuilder.java
@@ -0,0 +1,11 @@
+package com.example.ecommercemessagingschemaregistry;
+
+import org.springframework.messaging.Message;
+
+public class MessageBuilder<T> {
+
+    public Message<T> build() {
+        return new MessageBuilder<T>().build();
+
+    }
+}
